5959b5705714652868eb9133fd175117f4d4903c
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-01-27 15:06:32 +0100

    Adding annotation för user entity

--- DAL/Model/User.cs
+++ DAL/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace DAL
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public int Age { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
 
     }
