03a43b880dee4c9b0acb205f1cbf84c3c7b575ad
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-03-07 23:34:36 +0100

    Add DTO to get Budget & Category name  from frontend

---
+++ Service/DTOs/BudgetCategoryExpenseFilterDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTOs
+{
+    public class BudgetCategoryExpenseFilterDTO
+    {
+        public string BudgetName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
