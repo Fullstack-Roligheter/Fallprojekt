cfea4a741f8fdecdf9ddc4cda75194347ca7dede
Lei <babyonepucca@yahoo.com>
2022-03-17 11:36:16 +0100

    fix en till dto till create method

---
+++ Service/DTOs/SavingPlanDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTOs
+{
+    public class SavingPlanDTO
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public string PlanStartDate { get; set; }
+        public string PlanEndDate { get; set; }
+    }
+}
