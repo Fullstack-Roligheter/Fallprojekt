47f461dc5f26fda277519e982b289f9a6c206e05
Lei <babyonepucca@yahoo.com>
2022-03-17 11:37:19 +0100

    jag skapat en ny controll

---
+++ Fallprojekt/Controllers/SavingPlanController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using Service.DTOs;
+
+namespace Fallprojekt.Controllers
+{
+    [Route("api/saving")]
+    [ApiController]
+    public class SavingPlanController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateSavingPlan(SavingPlanDTO saving)
+        {
+            try
+            {
+                SavingPlanService.Instance.CreateSavingPlan(saving);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        public List<GetSavingPlanDTO>ListAllPlan()
+        {
+            return SavingPlanService.Instance.ListAllPlan();
+        }
+
+
+        //[HttpPost]
+        //public int GetDays(DateTime dateStart, DateTime dateEnd)
+        //{
+        //    return SavingPlanService.Instance.DateDiff(dateStart, dateEnd);
+        //}
+    }
+}
