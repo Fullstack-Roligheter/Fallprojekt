d90d0f389d56099eda2f7db3c65a8e38eed25a29
Lei <babyonepucca@yahoo.com>
2022-03-17 11:36:48 +0100

    I have a problem with listallplan

---
+++ Service/SavingPlanService.cs
@@ -0,0 +1,67 @@
+﻿using DAL;
+using DAL.Models;
+using Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class SavingPlanService
+    {
+        private static SavingPlanService _instance;
+        public static SavingPlanService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SavingPlanService();
+                }
+                return _instance;
+            }
+        }
+        public void CreateSavingPlan(SavingPlanDTO saving)
+        {
+            using (var context = new ProjectContext())
+            {
+                context.Add(
+                    new SavingPlan
+                    {
+                        Name = saving.Name,
+                        Amount = saving.Amount,
+                        PlanStartDate = saving.PlanStartDate,
+                        PlanEndDate = saving.PlanEndDate
+                    });
+                context.SaveChanges();
+            }
+        }
+        public List<GetSavingPlanDTO> ListAllPlan()
+        {
+            using (var context = new ProjectContext())
+            {
+                return context.Savingplan
+                    .Select(s => new GetSavingPlanDTO
+                    {
+                        Name = s.Name,
+                        PlanStartDate = s.PlanStartDate,
+                        PlanEndDate = s.PlanEndDate,
+                        CountDown = DateDiff(s.PlanStartDate, s.PlanEndDate)
+                    })
+                    .ToList();
+            }
+        }
+        //method som räknar days
+        public int DateDiff(string dateStart, string dateEnd)
+
+        {
+            DateTime start = Convert.ToDateTime(dateStart);
+            DateTime end = Convert.ToDateTime(dateEnd);
+            TimeSpan ts = end.Subtract(start);
+            return ts.Days + 1;
+        }
+
+    }
+}
