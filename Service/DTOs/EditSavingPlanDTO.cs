1352c15af47a101e51fa51f67f5b8901f3a8500e
Lachlan Morrison <lachlanmorrison@outlook.com>
2022-10-10 19:10:18 +0200

    Simplified Controller Inserted DateParse in Service Adjusted DTO

--- Service/DTOs/EditSavingPlanDTO.cs
+++ Service/DTOs/EditSavingPlanDTO.cs
@@ -11,7 +11,7 @@ namespace Service.DTOs
         public Guid SavingId { get; set; }
         public string Name { get; set; }
         public int Amount { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
     }
 }
