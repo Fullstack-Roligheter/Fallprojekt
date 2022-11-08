951205fc305669ddbe31401f8d003e562e046a46
Lachlan Morrison <lmorrison85@gmail.com>
2022-11-08 10:41:10 +0100

    Service-Repo till User tabellen klar

---
+++ DAL/Repositories/UserRepo.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories;
+
+public class UserRepo : IUserRepo
+{
+    private readonly ProjectContext _projectContext;
+
+    public UserRepo(ProjectContext projectContext)
+    {
+        _projectContext = projectContext;
+    }
+
+    public List<User> GetAllUsers()
+    {
+        return _projectContext.Users.ToList();
+    }
+
+    public User? GetUser(Guid id)
+    {
+        return _projectContext.Users.FirstOrDefault(u => u.Id == id);
+    }
+
+    public User? GetUser(string email)
+    {
+        return _projectContext.Users.FirstOrDefault(u => u.Email == email);
+    }
+
+    public void CreateUser(User user)
+    {
+        _projectContext.Users.Add(user);
+        _projectContext.SaveChanges();
+    }
+}
